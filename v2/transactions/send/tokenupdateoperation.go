a1f957acc90623c9b8e906cdcfdea02ac72d9e96
ksgotko <ksgotko@gmail.com>
2025-09-16 11:51:44 +0300

    Minor changes to tokenupdateoperation.go

--- v2/transactions/send/tokenupdateoperation.go
+++ v2/transactions/send/tokenupdateoperation.go
@@ -8,8 +8,7 @@ import (
 // TokenUpdateOperation
 // / Construct and sign a protocol level token update transaction consisting
 // / of the token update operations encoded in the given CBOR.
-func TokenUpdateOperation(signer v2.UpdateSigner, nonce v2.SequenceNumber,
+func TokenUpdateOperation(signer v2.UpdateSigner, nonce v2.SequenceNumber, effectiveTime v2.TransactionTime,
 	expiry v2.TransactionTime, tokenId v2.TokenID, operations v2.TokenOperations) (*v2.UpdateInstruction, error) {
-	updOp := construct.TokenUpdateOperation(nonce, expiry, expiry, tokenId, operations)
-	return updOp.Sign(signer)
+	return construct.TokenUpdateOperation(nonce, effectiveTime, expiry, tokenId, operations).Sign(signer)
 }
