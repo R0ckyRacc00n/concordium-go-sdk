d7d4a1e60d26361c3c8e7308406a80d4b28c5449
ksgotko <ksgotko@gmail.com>
2025-09-30 13:04:00 +0300

    Removed unused parameter in `send/TokenUpdateOperation`

--- v2/transactions/send/tokenupdateoperation.go
+++ v2/transactions/send/tokenupdateoperation.go
@@ -8,7 +8,7 @@ import (
 // TokenUpdateOperation
 // / Construct and sign a protocol level token update transaction consisting
 // / of the token update operations encoded in the given CBOR.
-func TokenUpdateOperation(sender v2.AccountAddress, signer v2.ExactSizeTransactionSigner, nonce v2.SequenceNumber, effectiveTime v2.TransactionTime,
+func TokenUpdateOperation(sender v2.AccountAddress, signer v2.ExactSizeTransactionSigner, nonce v2.SequenceNumber,
 	expiry v2.TransactionTime, tokenId v2.TokenID, operations v2.TokenOperations) (*v2.AccountTransaction, error) {
 	return construct.TokenUpdateOperation(signer.NumberOfKeys(), sender, nonce, expiry, tokenId, operations).Sign(signer)
 }
